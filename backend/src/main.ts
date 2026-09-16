cdf0fbd4d95f4bee340b7720874acc6f5d81dc9b
vansh43210 <tyagivansh45@gmail.com>
2026-09-16 23:27:40 +0530

    minor changes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module.js';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.API_URL
+    origin: process.env.FRONTEND_URL
   });
   await app.listen(process.env.PORT ?? 3000);
 }
